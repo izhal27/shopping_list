dd1312172a1302f6438cf232342c92ebd2b2d693
Risal Walangadi <risal.walangadi@gmail.com>
2024-03-14 08:32:28 +0800

    add new item widget

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,6 +14,58 @@ class _NewItemState extends State<NewItem> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Item'),
+      ),
+      body: Form(
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text("Name"),
+                ),
+                validator: (value) {
+                  return 'foo';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Amount"),
+                      ),
+                      initialValue: "1",
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [],
+                      onChanged: (item) {},
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Reset"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Add Item"),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
